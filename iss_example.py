efc60934dfa1c79c48774391f381f8280c62366c
jhacklander <jhacklander@gmail.com>
2024-07-03 19:04:55 -0700

    7 04 pm

--- iss_example.py
+++ iss_example.py
@@ -1,11 +1,10 @@
 import requests
 import json
 
-URI = "http://apiopen-notify.org/iss-now.json"
+URI = "http://api.open-notify.org/iss-now.json"
 
 resp = requests.get(URI)
 if resp.status_code == 200:
     json_data = json.dumps(resp.json(), indent=4)
     print(json_data)
 
-    
