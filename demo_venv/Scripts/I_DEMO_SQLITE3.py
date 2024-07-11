9f3ea5f287b6d347b8f5c96f90050af3f8b1fd64
jhacklander <jhacklander@gmail.com>
2024-07-10 19:30:06 -0700

    again

--- demo_venv/Scripts/I_DEMO_SQLITE3.py
+++ demo_venv/Scripts/I_DEMO_SQLITE3.py
@@ -1,3 +1,17 @@
 #import librarires
 import sqlite3 as sq
 from faker import Faker
+from tabulate import tabulate
+
+DATABASE_NAME = ':memory:'
+TABLE_NAME = 'tb_employees'
+TABLE_COL_ID = 'emp_id_pk'
+TABLE_COL_NAME = 'emp_name'
+MAX_EMPLOYEES = 10
+
+with sq.connect(DATABASE_NAME) as conn:
+    SQL = F'''CREATE TABLE IS NOT EXISTS {TABLE_NAME}
+    (
+            {TABLE_COL_ID} INTEGER PRIMARY KEY
+            {TABLE_COL_NAME} TEXT NOT NULL UNIQUE
+    );'''
