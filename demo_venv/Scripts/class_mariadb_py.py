037caaeaa73f229ff5739ac66a87c661f026d938
jhacklander <jhacklander@gmail.com>
2024-07-17 19:21:24 -0700

    721pm

--- demo_venv/Scripts/class_mariadb_py.py
+++ demo_venv/Scripts/class_mariadb_py.py
@@ -1,19 +1,23 @@
 import mariadb
 from tabulate import tabulate
 from dotenv import dotenv_values
+from sys import exit
 
 conn_parm = dotenv_values(".env")
 # for k, v in conn_parm.items():
 #     print(f'Key: {k} Value: {v}')
 conn = mariadb.connect(**conn_parm)
 with conn.cursor() as tables:
-    SQL = '''SELECT dept_id_pk 'ID',
-            dept_name 'DEPT',
-            dept_description,
-            dept_description,
-            dept_is_active 'ACTIVE'
-            FROM tb_dept
-            ORDER BY dept_name;   
-            '''
-    tables.execute(SQL)
-    print(tabulate(tables, headers=['DATABASES'], tablefmt = 'fancy_grid'))
+    data = ("Research and Development", "Research new product")
+    tb1 = "tb_dept"
+    col = "dept_name, dept_description"
+    SQL = f'''INSERT INTO {tb1} ({col}) VALUE (?, ?);'''
+    print(SQL)
+    try:
+        tables.execute(SQL, data=data)
+    except mariadb.Error as e:
+        print(f'Error: {e}')
+        exit(1)
+    SQL = '''SELECT * FROM vw_Active_Depts'''
+    print(tabulate(tables, headers=['Active_Depts'], tablefmt = 'fancy_grid'))
+conn.close()
