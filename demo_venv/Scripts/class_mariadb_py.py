564aecc08cffe3e53eb4b36b319acfad2d1d1e15
jhacklander <jhacklander@gmail.com>
2024-07-17 18:30:27 -0700

    7172024 630pm

--- demo_venv/Scripts/class_mariadb_py.py
+++ demo_venv/Scripts/class_mariadb_py.py
@@ -1,3 +1,12 @@
 import mariadb
 from tabulate import tabulate
 from dotenv import dotenv_values
+
+conn_parm = dotenv_values(".env")
+# for k, v in conn_parm.items():
+#     print(f'Key: {k} Value: {v}')
+conn = mariadb.connect(**conn_parm)
+with conn.cursor() as tables:
+    SQL = '''SHOW DATABASES;'''
+    tables.execute(SQL)
+    print(tabulate(tables, headers=['DATABASES'], tablefmt = 'fancy_grid'))
