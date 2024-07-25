2f62bc8447b065aafdb493f076b3954b8feda410
jhacklander <jhacklander@gmail.com>
2024-07-24 18:28:16 -0700

    edg

---
+++ demo_venv/Scripts/pymymariadb.py
@@ -0,0 +1,21 @@
+import pymysql.cursors
+from dotenv import load_dotenv
+from os import environ
+
+load_dotenv(".env")
+HOST = environ["host"]
+USER = environ["user"]
+PASSWORD = environ["password"]
+DATABASE = environ["database"]
+
+conn = pymysql.connect(host=HOST,
+                       user=USER,
+                       password=PASSWORD,
+                       database=DATABASE)
+with conn:
+    with conn.cursor() as curr:
+        SQL = '''SHOW TABLES'''
+        curr.execute(SQL)
+        tables = curr.fetchall()
+        for table in tables:
+            print(f'{tables}')
